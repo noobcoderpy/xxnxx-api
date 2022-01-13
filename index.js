3175b8ba0324ea0f96ac712b535962bfb56ac763
noobcoderpy <97080634+noobcoderpy@users.noreply.github.com>
2022-01-13 13:05:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-import express from "express"
+import express from "express";
+import { channel, TopList, trending, video } from "./controller/list.js"
 const app = express()
-app.get('/list/pornster/:num?', (req, res) => {
-    res.send('ok ' + req.params.num)
+app.get('/top/list/pornstars/:nam?', (req, res) => {
+    TopList(req.params.nam).then((r) => {
+        res.json(
+            r
+        )
+    })
 })
-app.listen(8080, () => {
-    console.log("ok");
+app.get('/channel/:name', (req, res) => {
+    channel("https://xhamster3.com/pornstars/" + req.params.name).then((r) => {
+        res.json(r)
+    })
 })
-
-
-// import {} from Cheerio
+app.get('/video', (req, res) => {
+    video(req.query.url).then((r) => {
+        res.json(r)
+    })
+})
+app.get('/', (_, res) => {
+    trending().then((r) => {
+        res.json(r)
+    })
+})
+app.listen(8083, () => {
+    console.log('ok');
+})
